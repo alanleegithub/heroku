b256a17dbb828819b8e469bb22e03caf490b0aa6
alan.lee <alanlee2005@gmail.com>
2014-08-27 17:30:03 +0800

    modify login function

--- blog/views.py
+++ blog/views.py
@@ -8,21 +8,6 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 
-def login(request):
-    #c = {}
-    #c.update(csrf(request))
-    #return render_to_response('login.html')#, c)
-    
-    email = request.POST.get('email', '')
-    password = request.POST.get('password', '')
-    user = auth.authenticate(email=email, password=password)
-
-    if user is not None:
-        auth.login(request, user)
-        return HttpResponseRedirect('blogs.html')
-    else:
-        return render_to_response('login.html')
-
 def blogs(request):
     return render_to_response('blogs.html',
         {'blogs': Post.objects.all()},  context_instance=RequestContext(request))
@@ -38,23 +23,30 @@ def tagpage(request, tag):
 
 from django.contrib.auth.forms import UserCreationForm
 
-def register_user(request):
-    # 2nd time around
+def login(request):
+    email = request.POST.get('email', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(email=email, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('blogs.html')
+    else:
+        return HttpResponseRedirect('/register/main/')
+
+def register_main(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('register.html',  c)
+
+def register_check(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/accounts/register_success')
-
-    # 1st time visit
-    args = {}
-    args.update(csrf(request))
-
-    # form with no input
-    args['form'] = UserCreationForm()
-    print args
-
-    return render_to_response('register.html', args)
+            return HttpResponseRedirect('/blogs/')
+        else:
+            return HttpResponseRedirect('/register/main/')
 
 def register_success(request):
     return render_to_response('register_success.html')
