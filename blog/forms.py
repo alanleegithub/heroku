89be57c7c37716ada766cdf1f18c70acf7e0f6be
alan.lee <alanlee2005@gmail.com>
2014-09-05 13:54:44 +0800

    fix do not save "password"

--- blog/forms.py
+++ blog/forms.py
@@ -24,7 +24,8 @@ class MyRegistrationForm(UserCreationForm):
         user.email = self.cleaned_data['email']
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
-
+        user.set_password(self.cleaned_data['password1'])
+        
         if commit:
             user.save()
 
